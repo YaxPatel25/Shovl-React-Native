2c58a66ae883a55a0dc7eea9b22c863a1b7c8a5e
nisalpadukka <nisalnadeera@gmail.com>
2022-07-16 02:20:41 -0400

    Initial migration

---
+++ ios/Pods/Target Support Files/Pods-shovl_rn_cli-shovl_rn_cliTests/Pods-shovl_rn_cli-shovl_rn_cliTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_shovl_rn_cli_shovl_rn_cliTests : NSObject
+@end
+@implementation PodsDummy_Pods_shovl_rn_cli_shovl_rn_cliTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-shovl_rn_cli/Pods-shovl_rn_cli-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_shovl_rn_cli : NSObject
+@end
+@implementation PodsDummy_Pods_shovl_rn_cli
+@end
