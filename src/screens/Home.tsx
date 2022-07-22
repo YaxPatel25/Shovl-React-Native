5676d5852c750e1c0539a5b661edd47854d292ae
nisalpadukka <nisalnadeera@gmail.com>
2022-07-22 16:05:04 -0400

    Use effect added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, Image, ScrollView, Alert } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 
@@ -7,25 +7,22 @@ import { FlatList } from "react-native-gesture-handler";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 
-//const HomeTabStack = createNativeStackNavigator();
-
-
-const HomeScreen = ({ navigation, route}: {navigation: any,route: any}) => {
-  // const username = route.params.username;
-
-  
-  const [posts, setPosts] = useState([
+const samplePosts = [
 	{ id:1, image: require('./../../assets/images/snowAdvertise.jpeg'), title: "Shoveler Needed", location: "22, Georgian Drive",postedBy:"User 1", description:"Desc 1" },
   { id:2,image: require('./../../assets/images/1.jpeg'), title: "Required Shoveler", location: "35, Maple Drive",postedBy:"User 2", description:"Desc 1" },
   { id:3,image: require('./../../assets/images/1.jpeg'), title: "Required Shoveler", location: "35, Maple Drive",postedBy:"User 3" , description:"Desc 1"},
   { id:4,image: require('./../../assets/images/2.jpeg'), title: "Hiring Hiring Hiring", location: "Barrie, Ontario" ,postedBy:"User 4", description:"Desc 1"},
   { id:5,image: require('./../../assets/images/3.jpeg'), title: "Snow Removal", location: "244, Georgian Drive" ,postedBy:"User 5", description:"Desc 1"},
   { id:6,image: require('./../../assets/images/4.jpeg'), title: "Snow Clearing needed", location: "21 Emcarr Drive" ,postedBy:"User 6", description:"Desc 1"},
-  { id:7,image: require('./../../assets/images/5.jpeg'), title: "Shovelers required", location: "30 Glenda Road" ,postedBy:"User 7", description:"Desc 1"},
-  
-]); 
+  { id:7,image: require('./../../assets/images/5.jpeg'), title: "Shovelers required", location: "30 Glenda Road" ,postedBy:"User 7", description:"Desc 1"}
+];
 
-       const signout = () => {
+
+const HomeScreen = ({ navigation, route}: {navigation: any,route: any}) => {
+
+  const [posts, setPosts] = useState(samplePosts); 
+
+  const signout = () => {
         Alert.alert("Attention \n", " Do you really want to sign out!", [
           {
             text: "Yes",
@@ -44,7 +41,25 @@ const HomeScreen = ({ navigation, route}: {navigation: any,route: any}) => {
             style: "cancel",
           },
         ]);
-      };
+  };
+
+  const renderRow = ({item}) => {
+    return (
+    <View style={styles.item}>
+    <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate("AdvertiseDetails",item)} >
+                  <Image source={item.image}
+                        style={{width: 130, height:100}}/>
+                    <Text style={styles.titleText}>{item.title}</Text>
+                    <Text style={styles.locationText}>{item.location}</Text>
+    </TouchableOpacity>
+
+  </View>);
+};
+
+  useEffect(() => {
+    //populating Post data
+    setPosts(samplePosts)
+  });
 
   return (
 
@@ -62,20 +77,9 @@ const HomeScreen = ({ navigation, route}: {navigation: any,route: any}) => {
             
             <FlatList
             numColumns={2}
-            data={
-              posts
-            }
-            renderItem={({item})=>(
-                    <View style={styles.item}>
-                      {<TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate("AdvertiseDetails",item)} >
-                                    <Image source={item.image}
-                                          style={{width: 130, height:100}}/>
-                                      <Text style={styles.titleText}>{item.title}</Text>
-                                      <Text style={styles.locationText}>{item.location}</Text>
-                      </TouchableOpacity>}
-
-                    </View>
-            )}/>
+            data={ posts }
+            renderItem={renderRow}
+            />
             </View>
       </View>
 </SafeAreaView>)
